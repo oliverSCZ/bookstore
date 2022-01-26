bf6acc643c284b723943d89af0a83dbcef4c9777
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-26 10:44:37 -0400

    Fix issue with the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,9 @@
-import { createStore, combineReducers, applyMiddleware }from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import books from './books/books';
 
 const reducer = combineReducers({
-  books,  
+  books,
 });
 
 const store = createStore(reducer, applyMiddleware(logger));
