67745a8ef5cf10863adf828199b667dc218d1ba0
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-27 14:53:15 -0400

    Fix duplicate books

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import NavBar from './components/NavBar';
 import BookShelf from './components/BookShelf';
 import Categories from './components/Categories';
+import { getInitialBooks } from './redux/books/booksRedux';
 
-const App = () => (
+const App = () =>{
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getInitialBooks());
+  }, []);
+
+  return (
   <div className="App">
     <NavBar />
     <Routes>
@@ -11,6 +20,6 @@ const App = () => (
       <Route exact path="/categories" element={<Categories />} />
     </Routes>
   </div>
-);
+)};
 
 export default App;
