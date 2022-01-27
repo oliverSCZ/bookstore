1de2681fadcd09d147a1aff07ae4ac81850f552e
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-27 15:06:21 -0400

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import BookShelf from './components/BookShelf';
 import Categories from './components/Categories';
 import { getInitialBooks } from './redux/books/booksRedux';
 
-const App = () =>{
+const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getInitialBooks());
   }, []);
 
   return (
-  <div className="App">
-    <NavBar />
-    <Routes>
-      <Route exact path="/" element={<BookShelf />} />
-      <Route exact path="/categories" element={<Categories />} />
-    </Routes>
-  </div>
-)};
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<BookShelf />} />
+        <Route exact path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
